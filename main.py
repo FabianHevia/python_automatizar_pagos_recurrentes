c78c598d8fa18e64f74a7279909d5df4c48f9ae7
Fabi <141791420+FabianHevia@users.noreply.github.com>
2025-12-01 20:13:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+'''
+
+Dado un monto inicial y una lista de fechas, 
+genera un calendario de pagos con montos iguales.
+
+'''
+
 monto = 50000
 fechas = ["2025-01-10", "2025-02-10", "2025-03-10"]
 
+# El monto se divide en cuantas fechas de pago existen con un mismo monto
 pago = monto / len(fechas)
+
+'''
+
+Entonces se crea un calendario el cual es capaz de según cada fecha en fechas,
+asignar una fecha y un monto a cada pago redondeando en dos decimales.
+
+'''
 calendario = [{ "fecha": f, "monto": round(pago, 2) } for f in fechas]
 
 print(calendario)
